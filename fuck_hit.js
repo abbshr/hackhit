839353e3fdbe4ba0b9639d4d10ac184f13dbc38b
Soufii <abbshr@outlook.com>
2014-03-11 21:28:41 +0800

    update version, add --ios and --web options to run mode

--- fuck_hit.js
+++ fuck_hit.js
@@ -1,7 +1,10 @@
 /* fuck HIT Server-Proxy */
-/*      beta v0.1.1      */
-
+/*      beta v0.1.2      */
+var argv = process.argv;
 var http   = require('http');
 var router = require('./router.js');
-
+if (argv[2] == '--web')
+    router = require('./router.js');
+if (argv[2] == '--ios')
+    router = require('./for_ios_client.js');
 http.createServer(router).listen(3000);
